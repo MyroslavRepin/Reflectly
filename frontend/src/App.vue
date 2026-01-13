2935280497edaac62ab3029bc7284895e8b438c5
Myroslav Repin <myroslavrepin@gmail.com>
2026-01-12 20:32:17 -0700

    inti: basic Vue structure

---
+++ frontend/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import ExternalApi from "./components/ExternalApi.vue";
+</script>
+
+<template>
+<external-api></external-api>
+</template>
+
+<style scoped>
+</style>
---
+++ frontend/src/components/ExternalApi.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+let count = ref(0)
+const serverUrl = "https://api.api-ninjas.com/v2/quotes"
+
+async function fetchQuote() {
+  const response = await fetch(serverUrl, {
+    method: "GET",
+  })
+  const data = await response.json()
+  return data
+}
+let quote = ref(await fetchQuote())
+</script>
+
+<template>
+  <button @click="count++">{{quote}}</button>
+  <h1>{{}}</h1>
+</template>
+
+<style scoped>
+
+</style>
