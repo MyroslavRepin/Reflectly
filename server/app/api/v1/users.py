c659535f8585c126be33ae002f00b995f6b02fce
Myroslav Repin <myroslavrepin@gmail.com>
2026-02-07 13:25:32 -0700

    feat: add `is_deleted` field to users schema and improve user API
    - Added `is_deleted` column to `users` table with migration.
    - Updated user model and repository to support `is_deleted` field.
    - Extended `/api/v1/users/me` endpoint to return user details with response model `UserResponse`, including the `is_deleted` attribute.
    - Implemented password hashing for updates in `/api/v1/users/me`.
    - Added a new `/api/v1/users/me` GET endpoint to fetch current user details.
    
    Signed-off-by: Myroslav Repin <myroslavrepin@gmail.com>

--- server/app/api/v1/users.py
+++ server/app/api/v1/users.py
@@ -1,3 +1,5 @@
+from http.client import HTTPException
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -5,15 +7,36 @@ from server.db.repositories.users import UserRepository
 from server.db.sessions import get_db
 from server.deps.auth_deps import get_current_user
 from server.core.logging_config import logger
-from server.deps.schemas.users_schemes import UpdateUser
+from server.deps.schemas.users_schemes import UpdateUser, UserResponse
+from server.utils.security.auth import hash_password
 
 router = APIRouter()
 
 user_repo = UserRepository()
 
-@router.patch("/api/v1/users/me")
-async def update_user(user_data: UpdateUser, db: AsyncSession = Depends(get_db), jwt_decoded: str = Depends(get_current_user)):
+@router.patch("/api/v1/users/me", response_model=UserResponse)
+async def update_user(
+    user_data: UpdateUser,
+    db: AsyncSession = Depends(get_db),
+    jwt_decoded: dict = Depends(get_current_user),
+):
     user_id = int(jwt_decoded["user_id"])
-    logger.debug(f"Updating user {jwt_decoded['user_id']}")
-    user = await user_repo.update(db, user_id, **user_data.dict())
+    logger.debug(f"Updating user {user_id}")
+
+    update_data = user_data.dict(exclude_unset=True)
+
+    if "password" in update_data:
+        update_data["hashed_password"] = hash_password(
+            plain_password=update_data.pop("password")
+        )
+
+    user = await user_repo.update(db, user_id, **update_data)
+    return user
+
+@router.get("/api/v1/users/me", response_model=UserResponse)
+async def get_current_user(jwt_decoded: str = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
+    user_id = int(jwt_decoded["user_id"])
+    user = await user_repo.get_user_by_id(db, user_id)
+    if not user:
+        HTTPException(status_code=404, detail="User not found")
     return user
--- server/deps/schemas/users_schemes.py
+++ server/deps/schemas/users_schemes.py
@@ -21,3 +21,12 @@ class UpdateUser(BaseModel):
     username: Optional[str] = None
     email: Optional[EmailStr] = None
     password: Optional[str] = None
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+
+    model_config = {
+        "from_attributes": True
+    }
